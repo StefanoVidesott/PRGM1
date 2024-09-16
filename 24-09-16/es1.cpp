d64586dfb515e84c800d25bfdbf6af2645c2b016
Videsott Stefano (ACC0191370) <stefano.videsott@sci-b106pc015x.unitn.it>
2024-09-16 09:26:21 +0200

    Edited es1.cpp

--- 24-09-16/es1.cpp
+++ 24-09-16/es1.cpp
@@ -4,6 +4,15 @@
 
 int main() {
 
+    char character;
+
+    std::cout << "Enter a character: ";
+    std::cin >> character;
+    std::cout << character << " is the character entered" << std::endl;
+
     return 0;
 
 }
+
+// echo "z" | ./es
+/* | invia informazioni al processo successivo */
